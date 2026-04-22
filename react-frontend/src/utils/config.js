fba8dd0cc04fcf1b7fe7678f72d517f442e4879b
makkhemara-collab <makkhemara@gmail.com>
2026-04-22 14:12:28 +0700

    Fix double slash URL bug

--- react-frontend/src/utils/config.js
+++ react-frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 // This MUST be the Backend (the -dzlc link), NOT the frontend
-export const baseURL = "https://fullstackproject1-1-dzlc.onrender.com/";
+export const baseURL = "https://fullstackproject1-1-dzlc.onrender.com";
 
 export const getImagePath = (path) => {
   if (!path) return "https://images.unsplash.com/photo-1541167760496-1628856ab772?q=80&w=500";
